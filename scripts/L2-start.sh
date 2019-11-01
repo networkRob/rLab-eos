1c75b5ae8642e3264dfcc35f5bd147a456ed08ca
Rob Martin <robmartin@arista.com>
2019-11-01 16:10:24 -0500

    l2 test

---
+++ scripts/L2-start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+sudo ovs-vsctl add-br l2spine1spine2
+sudo ovs-vsctl set bridge l2spine1spine2 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2spine1leaf1
+sudo ovs-vsctl set bridge l2spine1leaf1 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2spine1leaf2
+sudo ovs-vsctl set bridge l2spine1leaf2 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2spine1leaf3
+sudo ovs-vsctl set bridge l2spine1leaf3 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2spine2leaf1
+sudo ovs-vsctl set bridge l2spine2leaf1 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2spine2leaf2
+sudo ovs-vsctl set bridge l2spine2leaf2 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2spine2leaf3
+sudo ovs-vsctl set bridge l2spine2leaf3 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2leaf1host10
+sudo ovs-vsctl set bridge l2leaf1host10 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2leaf1host11
+sudo ovs-vsctl set bridge l2leaf1host11 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2leaf2host20
+sudo ovs-vsctl set bridge l2leaf2host20 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2leaf2host21
+sudo ovs-vsctl set bridge l2leaf2host21 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2leaf3host30
+sudo ovs-vsctl set bridge l2leaf3host30 other-config:forward-bpdu=true
+sudo ovs-vsctl add-br l2leaf3host31
+sudo ovs-vsctl set bridge l2leaf3host31 other-config:forward-bpdu=true
+docker create --name=l2spine1 --privileged -v $(pwd)/configs/l2spine1:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceosimage:4.22.1F.Trunk /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker start l2spine1
+ovs-docker add-port l2spine1spine2 eth1 l2spine1 --macaddress=00:1c:73:c0:c6:01
+docker create --name=l2spine2 --privileged -v $(pwd)/configs/l2spine2:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceosimage:4.22.1F.Trunk /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker start l2spine2
+ovs-docker add-port l2spine1spine2 eth1 l2spine2 --macaddress=00:1c:73:c1:c6:01
+docker create --name=l2leaf1 --privileged -v $(pwd)/configs/l2leaf1:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceosimage:4.22.1F.Trunk /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker start l2leaf1
+ovs-docker add-port l2spine1leaf1 eth1 l2leaf1 --macaddress=00:1c:73:c2:c6:01
+docker create --name=l2leaf3 --privileged -v $(pwd)/configs/l2leaf3:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceosimage:4.22.1F.Trunk /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker start l2leaf3
+ovs-docker add-port l2spine1leaf3 eth1 l2leaf3 --macaddress=00:1c:73:c4:c6:01
+docker create --name=l2leaf2 --privileged -v $(pwd)/configs/l2leaf2:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceosimage:4.22.1F.Trunk /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker start l2leaf2
+ovs-docker add-port l2spine1leaf2 eth1 l2leaf2 --macaddress=00:1c:73:c3:c6:01
---
+++ scripts/topo-builder.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import argparse
+from ruamel.yaml import YAML
+from os import getcwd
+from pprint import pprint as pp
+
+BASE_PATH = getcwd()
+CONFIGS = BASE_PATH + "/configs/"
+
+OVS_BRIDGES = []
+NODES = {}
+CMDS = []
+
+def openTopo(topo):
+    try:
+        tmp_topo = open(BASE_PATH + "/topologies/{}.yaml".format(topo), 'r')
+        tmp_yaml = YAML().load(tmp_topo)
+        tmp_topo.close()
+        return(tmp_yaml)
+    except:
+        return(False)
+
+def checkBridge(dev1, dev2):
+    global OVS_BRIDGES
+    _addBr = True
+    for _br in OVS_BRIDGES:
+        if dev1 in _br and dev2 in _br:
+            _addBr = False
+            return({'status': _addBr, 'name': _br})
+    return({'status': _addBr, 'name':''})
+
+
+def main(args):
+    global OVS_BRIDGES, NODES
+    topo_yaml = openTopo(args.type)
+    if topo_yaml:
+        _tag = topo_yaml['topology']['name']
+        for _node in topo_yaml['nodes']:
+            intf = 0
+            NODES[_node] = {
+                "intfs": {},
+                "name": _tag.lower() + _node
+            }
+            for _peer in topo_yaml['nodes'][_node]['links']:
+                intf += 1
+                _brCheck = checkBridge(_node, _peer)
+                if _brCheck['status']:
+                    _brName = _tag.lower() + _node + _peer
+                    OVS_BRIDGES.append(_brName)
+                    NODES[_node]['intfs']['eth{}'.format(intf)] = _brName
+                else:
+                    NODES[_node]['intfs']['eth{}'.format(intf)] = _brCheck['name']
+    # Create commands to create Open vSwitch bridges
+    for _br in OVS_BRIDGES:
+        CMDS.append("sudo ovs-vsctl add-br {}".format(_br))
+        CMDS.append("sudo ovs-vsctl set bridge {} other-config:forward-bpdu=true".format(_br))
+    # Create commands to create cEOS containers:
+    for _node in NODES:
+        CMDS.append("docker create --name={0} --privileged -v $(pwd)/configs/{1}:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceosimage:{2} /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker".format(NODES[_node]['name'], NODES[_node]['name'], args.image))
+        CMDS.append("docker start {}".format(NODES[_node]['name']))
+        for eindex in range(1, len(NODES[_node]['intfs']) + 1):
+            if eindex == 1:
+                CMDS.append("ovs-docker add-port {0} eth{1} {2} --macaddress={3}".format(NODES[_node]['intfs']['eth{}'.format(eindex)], eindex, NODES[_node]['name'], topo_yaml['nodes'][_node]['mac']))
+    if CMDS:
+        with open(BASE_PATH + "/scripts/{}-start.sh".format(_tag), 'w') as fout:
+            fout.write("#!/bin/bash\n")
+            for _cmd in CMDS:
+                fout.write(_cmd + "\n")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--type", type=str, help="Topology to build", default=None, required=True)
+    parser.add_argument("-i", "--image", type=str, help="cEOS image to use", default="4.22.1F.Trunk", required=False)
+    # parser.add_argument("-u", "--user", type=str, help="User to login with", default=None, required=True)
+    # parser.add_argument("-s", "--setup", type=str, help="User to use for AAA setup", default=None, required=False)
+    # parser.add_argument("-d","--debug", action="store_true", help="Print log messages.", required=False)
+    args = parser.parse_args()
+    main(args)
