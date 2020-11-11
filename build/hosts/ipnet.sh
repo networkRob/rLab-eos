e6f2a43178cc1ad893da6ad6f8fe4f7fb276203a
Rob Martin <robmartin@arista.com>
2020-11-11 08:42:50 -0600

    updated host build

--- build/hosts/ipnet.sh
+++ build/hosts/ipnet.sh
@@ -2,8 +2,20 @@
 
 echo "PS1=\"\u@$HOSTNAME# \"" >> ~/.bashrc
 
+echo "$HOSTNAME" > /etc/hostname
+
 ifconfig et0 $HOST_IP netmask $HOST_MASK
 
 route add default gw $HOST_GW et0
 
+lldpap -d 
+for i in `ls /sys/class/net/ | grep et` ;
+      do echo "enabling lldp for interface: $i" ;
+      lldptool set-lldp -i $i adminStatus=rxtx  ;
+      lldptool -T -i $i -V  sysName enableTx=yes;
+      lldptool -T -i $i -V  portDesc enableTx=yes ;
+      lldptool -T -i $i -V  sysDesc enableTx=yes;
+      lldptool -T -i $i -V sysCap enableTx=yes;
+      lldptool -T -i $i -V mngAddr enableTx=yes;
+done
 tail -f /dev/null
